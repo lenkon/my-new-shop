15469b479f6451eef8ba45a1dec87933e83f011b
inoulen inoulen <inoulenkon@gmail.com>
2023-12-29 20:30:26 +0530

    Pass products state as Prop to Products component

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,15 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Loading from './Loading';
 import Product from './Product';
 
 export default function Cart() {
   const [product, setProduct] = useState([]);
+  useEffect(() => {
+    
+    
+  }, []);
 
   return (
     <div className='container m-auto text-center'>
       <h1 style={{color:'#9e74c4'}}>Product List</h1>
       {
-        product.length === 0 ? <Loading /> : <Product />
+        product.length === 0 ? 
+        <Loading /> : 
+        product.map((item, index) => { 
+          return <Product key={index} item = {item} />})
       }
     </div>
   )
--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Product() {
+function Product({item}) {
   return (
     <div>
       Product
