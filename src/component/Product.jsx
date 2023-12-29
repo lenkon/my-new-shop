ffe39dbb7a0e0772567c4871fe8281323840a65b
inoulen inoulen <inoulenkon@gmail.com>
2023-12-29 20:53:26 +0530

    Display product details

--- src/component/Product.jsx
+++ src/component/Product.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function Product({item}) {
+  // console.log(item)
+  const {title, image, description, price} = item;
   return (
     <div>
-      Product
+      <h1>{title}</h1>
+      <img src={image}></img>
+      <h3>{price}</h3>
+      <h5>{description}</h5>
     </div>
   )
 }
