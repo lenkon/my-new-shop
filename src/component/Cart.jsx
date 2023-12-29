ee8a23416242dd3fdc6f1b23946e422f7d3653ac
inoulen inoulen <inoulenkon@gmail.com>
2023-12-29 18:11:10 +0530

    Import and initialize useState

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Cart() {
+  const [product, setProduct] = useState([]);
+
   return (
     <div className='container m-auto text-center'>
       <h1 style={{color:'#9e74c4'}}>Product List</h1>
+      {
+      }
     </div>
   )
 }
