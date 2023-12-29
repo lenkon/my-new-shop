b3e92eccfde0e9011afdf4f4c1856ebad898d8bf
inoulen inoulen <inoulenkon@gmail.com>
2023-12-29 17:44:27 +0530

    Add header for product list

--- src/component/Cart.jsx
+++ src/component/Cart.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Cart() {
   return (
-    <div>
-      
+    <div className='container m-auto text-center'>
+      <h1 style={{color:'#9e74c4'}}>Product List</h1>
     </div>
   )
 }
